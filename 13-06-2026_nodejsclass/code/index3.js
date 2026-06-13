6101534f06433f954e42d4d3a186e99b7a6abd5b
sriramNodejs <dsriram50404@gmail.com>
2026-06-13 20:35:29 +0530

    node fs classes

---
+++ 13-06-2026_nodejsclass/code/index3.js
@@ -0,0 +1,10 @@
+// const index2 = require('./index2')
+const {sum: sum23, multiply, divide} = require('./index2')
+
+
+// index2.sum();
+// index2.multiply();
+// index2.divide();
+
+sum23()
+multiply();
